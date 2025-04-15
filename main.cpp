fed9ac0977e35e4a6b7cc81c211005b32e8357c8
arimizuyuuta <k024g1028@g.neec.ac.jp>
2025-04-15 11:37:59 +0900

    Demo

--- main.cpp
+++ main.cpp
@@ -12,6 +12,47 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
+
+	// 加算
+	Vector3 Add(const Vector3 & v1, const Vector3 & v2);
+	// 減算
+	Vector3 Subtract(const Vector3 & v1, const Vector3 & v2);
+	// スカラー倍
+	Vector3 Multiply(float scalar, const Vector3 & v);
+	// 内積
+	float Dot(const Vector3 & v1, const Vector3 & v2);
+	// 長さ(ノルム)
+	float Length(const Vector3 & v);
+	// 正規化
+	Vector3 Normalize(const Vector3 & v);
+
+	static const int kColumnWidth = 60;
+	void VectorScreenPrintf(int x, int y, const Vector3 & vector, const char* label) {
+	Novice: : ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice: : ScreenPrintf(x + kColumnWidth, y, "%. 02f", vector.y);
+	Novice: : ScreenPrintf(x + kColumnWidth * 2, y, "%. 02f", vector.z);
+	Novice: : ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+	}
+
+	Vector3 v1{ 1.0f, 3.0f, -5.0f };
+	Vector3 v2{ 4.0f, -1.0f, 2.0f };
+	float k = { 4.0f };
+
+	Vector3 resultAdd = Add(v1, v2);
+	Vector3 resultSubtract = Subtract(v1, v2);
+	Vector3 resultMultiply = Multiply(k, v1);
+	float resultDot = Dot(v1, v2);
+	float resultLength = Length(v1);
+	Vector3 resultNormalize = Normalize(v2);
+
+	VectorScreenPrintf(0, 0, resultAdd, " : Add");
+	VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
+	VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " : Multiply");
+Novice: : ScreenPrintf(0, kRowHeight * 3, "%. 02f : Dot", resultDot);
+Novice: : ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
+	VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " : Normalize");
+
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
